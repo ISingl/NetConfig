8da8db4c8bcf4759ea210a208af66b33695e2e83
Nogin Sergey <ver-s_online@mail.ru>
2020-09-07 02:56:37 +0400

    initial project version

---
+++ NetConfig/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace NetConfig
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnDynamic = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelDHCP = new System.Windows.Forms.Label();
+            this.btnDeleteIp = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAddIp = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtMask = new System.Windows.Forms.TextBox();
+            this.txtIp = new System.Windows.Forms.TextBox();
+            this.btnStatic = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnDynamic
+            // 
+            this.btnDynamic.Location = new System.Drawing.Point(254, 201);
+            this.btnDynamic.Name = "btnDynamic";
+            this.btnDynamic.Size = new System.Drawing.Size(132, 23);
+            this.btnDynamic.TabIndex = 0;
+            this.btnDynamic.Text = "Включить DHCP IP";
+            this.btnDynamic.UseVisualStyleBackColor = true;
+            this.btnDynamic.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.labelDHCP);
+            this.groupBox1.Controls.Add(this.btnDeleteIp);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(394, 112);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Настройка интерфейса Ethernet";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(147, 61);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "unknown";
+            // 
+            // labelDHCP
+            // 
+            this.labelDHCP.AutoSize = true;
+            this.labelDHCP.Location = new System.Drawing.Point(147, 21);
+            this.labelDHCP.Name = "labelDHCP";
+            this.labelDHCP.Size = new System.Drawing.Size(51, 13);
+            this.labelDHCP.TabIndex = 4;
+            this.labelDHCP.Text = "unknown";
+            // 
+            // btnDeleteIp
+            // 
+            this.btnDeleteIp.Location = new System.Drawing.Point(258, 83);
+            this.btnDeleteIp.Name = "btnDeleteIp";
+            this.btnDeleteIp.Size = new System.Drawing.Size(130, 23);
+            this.btnDeleteIp.TabIndex = 3;
+            this.btnDeleteIp.Text = "Удалить IP-адрес";
+            this.btnDeleteIp.UseVisualStyleBackColor = true;
+            this.btnDeleteIp.Click += new System.EventHandler(this.BtnDeleteIp_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(150, 37);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(189, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Префикс подсети:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "IP - адрес:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DHCP:";
+            // 
+            // btnAddIp
+            // 
+            this.btnAddIp.Location = new System.Drawing.Point(42, 77);
+            this.btnAddIp.Name = "btnAddIp";
+            this.btnAddIp.Size = new System.Drawing.Size(116, 23);
+            this.btnAddIp.TabIndex = 4;
+            this.btnAddIp.Text = "Добавить IP-адрес";
+            this.btnAddIp.UseVisualStyleBackColor = true;
+            this.btnAddIp.Click += new System.EventHandler(this.BtnAddIp_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.btnAddIp);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.txtMask);
+            this.groupBox2.Controls.Add(this.txtIp);
+            this.groupBox2.Location = new System.Drawing.Point(12, 140);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(210, 109);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Дополнительно";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(19, 51);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(43, 13);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Маска:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(19, 25);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "IP-адрес:";
+            // 
+            // txtMask
+            // 
+            this.txtMask.Location = new System.Drawing.Point(78, 48);
+            this.txtMask.Name = "txtMask";
+            this.txtMask.Size = new System.Drawing.Size(116, 20);
+            this.txtMask.TabIndex = 1;
+            // 
+            // txtIp
+            // 
+            this.txtIp.Location = new System.Drawing.Point(78, 22);
+            this.txtIp.Name = "txtIp";
+            this.txtIp.Size = new System.Drawing.Size(116, 20);
+            this.txtIp.TabIndex = 0;
+            // 
+            // btnStatic
+            // 
+            this.btnStatic.Location = new System.Drawing.Point(254, 165);
+            this.btnStatic.Name = "btnStatic";
+            this.btnStatic.Size = new System.Drawing.Size(132, 23);
+            this.btnStatic.TabIndex = 7;
+            this.btnStatic.Text = "Включить STATIC IP";
+            this.btnStatic.UseVisualStyleBackColor = true;
+            this.btnStatic.Click += new System.EventHandler(this.BtnStatic_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 259);
+            this.Controls.Add(this.btnStatic);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnDynamic);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NetConfig ";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDynamic;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelDHCP;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnDeleteIp;
+        private System.Windows.Forms.Button btnAddIp;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtMask;
+        private System.Windows.Forms.TextBox txtIp;
+        private System.Windows.Forms.Button btnStatic;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ NetConfig/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetConfig
+{
+    public partial class Form1 : Form
+    {
+        List<string> currentIP = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text += GetAndDisplayRights();
+            GetConfigEthernet();            
+        }
+
+        private string GetAndDisplayRights()
+        {
+            WindowsPrincipal pricipal = new WindowsPrincipal(WindowsIdentity.GetCurrent());
+            bool hasAdministrativeRight = pricipal.IsInRole(WindowsBuiltInRole.Administrator);
+
+            return hasAdministrativeRight ? "(Running with full rights)" : "(Running with limited rights)";
+        }
+
+        private void GetConfigEthernet()
+        {
+            currentIP.Clear();
+            comboBox1.Items.Clear();
+            string[] temp = null;
+            temp = GetProcessCMD("/c netsh interface ipv4 show config name=\"Ethernet\"");
+
+            if (temp == null)
+                return;
+
+            string data;
+            for (int i = 0; i < temp.Length; i++)
+            {
+
+                if (temp[i].Contains("DHCP включен"))
+                {
+                    labelDHCP.Text = temp[i].Split(':')[1].Trim();
+                }
+                if (temp[i].Contains("IP-адрес"))
+                {
+                    data = temp[i].Split('с')[1].Trim() + "&";
+                    data += temp[i + 1].Split(':')[1].Trim();
+                    currentIP.Add(data);
+                }
+            }
+
+            foreach (string curr in currentIP)
+            {
+                comboBox1.Items.Add(curr.Split('&')[0]);
+            }
+            if (comboBox1.Items.Count != 0)
+                comboBox1.SelectedIndex = 0;
+            if (labelDHCP.Text == "Нет")
+            {
+                groupBox2.Enabled = true;
+                btnDeleteIp.Enabled = true;
+                comboBox1.Enabled = true;
+            }
+            if (labelDHCP.Text == "Да")
+            {
+                groupBox2.Enabled = false;
+                btnDeleteIp.Enabled = false;
+                comboBox1.Enabled = false;
+                label5.Text = "unknown";
+            }
+        }
+
+        private static string[] GetProcessCMD(string argument)
+        {
+            string[] temp = null;
+            try
+            {
+                Process processCmd = new Process();
+                processCmd.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                processCmd.StartInfo.UseShellExecute = false;
+                processCmd.StartInfo.RedirectStandardOutput = true;
+                processCmd.StartInfo.CreateNoWindow = true;
+                processCmd.StartInfo.FileName = "cmd.exe";
+                processCmd.StartInfo.Arguments = argument;
+                processCmd.StartInfo.StandardOutputEncoding = Encoding.GetEncoding(866);
+                processCmd.Start();
+                StreamReader srIncoming = processCmd.StandardOutput;
+                temp = srIncoming.ReadToEnd().Split('\n');
+                processCmd.WaitForExit();
+            }
+            catch (Win32Exception) { }
+
+            return temp;
+        }
+
+        private static void SetProcessCMD(string argument)
+        {
+            try
+            {
+                Process processCmd = new Process();
+                processCmd.StartInfo.Verb = "runas";
+                processCmd.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                processCmd.StartInfo.CreateNoWindow = true;
+                processCmd.StartInfo.FileName = "cmd.exe";
+                processCmd.StartInfo.Arguments = argument;
+                processCmd.Start();
+                processCmd.WaitForExit();
+            }
+            catch (Win32Exception) { }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            SetProcessCMD("/c netsh interface ipv4 set address name=\"Ethernet\" source=dhcp");
+            //Thread.Sleep(4000);
+            //GetConfigEthernet();
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label5.Text = currentIP[comboBox1.SelectedIndex].Split('&')[1];
+        }
+
+        private void BtnAddIp_Click(object sender, EventArgs e)
+        {
+            if (txtIp.Text == "" || txtMask.Text == "")
+            {
+                MessageBox.Show("Нет данных", "Ошибка");
+                return;
+            }
+
+            SetProcessCMD("/c netsh interface ipv4 add address \"Ethernet\" " + txtIp.Text+ " " + txtMask.Text);
+
+            txtIp.Text = "";
+            txtMask.Text = "";
+            //Thread.Sleep(4000);
+
+            timer1.Enabled = true;
+        }
+
+        private void BtnDeleteIp_Click(object sender, EventArgs e)
+        {
+            SetProcessCMD("/c netsh interface ipv4 delete address \"Ethernet\" " + comboBox1.Items[comboBox1.SelectedIndex].ToString().Split('&')[0]);
+            GetConfigEthernet();
+            SaveToFile();
+        }
+
+        private void SaveToFile()
+        {
+            if(!File.Exists(Directory.GetCurrentDirectory() + "/IP.txt"))
+            {
+                MessageBox.Show("Файл с конфигурацией не найден", "Ошибка");
+                return;
+            }
+
+            try
+            {
+                File.WriteAllLines(Directory.GetCurrentDirectory() + "/IP.txt", currentIP);
+            }
+            catch (Win32Exception) { }
+        }
+
+        private void BtnStatic_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(Directory.GetCurrentDirectory() + "/IP.txt"))
+            {
+                MessageBox.Show("Файл с конфигурацией не найден", "Ошибка");
+                return;
+            }
+
+            try
+            {
+                currentIP.Clear();
+                currentIP.AddRange(File.ReadAllLines(Directory.GetCurrentDirectory() + "/IP.txt"));
+                string tmp;
+                foreach(string data in currentIP)
+                {
+                    tmp = "/c netsh interface ipv4 add address \"Ethernet\" " + data.Split('&')[0] + " " + data.Split('(')[1].Split(' ')[1].Split(')')[0];
+                    SetProcessCMD(tmp);
+                }
+            }
+            catch (Win32Exception) { }
+            timer1.Enabled = true;
+            //Thread.Sleep(4000);
+            //GetConfigEthernet();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            GetConfigEthernet();
+            if (label5.Text == "Нет")
+                SaveToFile();
+            timer1.Enabled = false;
+        }
+    }
+}
